6307cbc6da8d8a0be83300bc30b491148c3d8634
tristeybro <the.tristanity.bena@gmail.com>
2018-01-22 19:15:59 -0500

    First commit. Setting up simple smtp emailer.

---
+++ credentials/credentials.go
@@ -0,0 +1,27 @@
+package credentials
+
+import (
+	"encoding/json"
+  "fmt"
+	"io/ioutil"
+  "os"
+)
+
+type EmailCredentials struct {
+    Email string
+    Password string
+}
+
+func importCredentials(pathToCredentials string) EmailCredentials {
+	raw, err := ioutil.ReadFile(pathToCredentials)
+  if err != nil {
+      fmt.Println(err.Error())
+      os.Exit(1)
+  }
+
+  var emailCredentials EmailCredentials
+  json.Unmarshal(raw, &emailCredentials)
+  return emailCredentials
+}
+
+var Credentials = importCredentials("credentials/credentials.json")
