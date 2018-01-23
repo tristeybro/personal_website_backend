733de6eae5e130a6d68193ca099aa1a8d9eaa2b5
tristeybro <the.tristanity.bena@gmail.com>
2018-01-22 19:21:36 -0500

    Formatting go files correctly.

--- credentials/credentials.go
+++ credentials/credentials.go
@@ -2,26 +2,26 @@ package credentials
 
 import (
 	"encoding/json"
-  "fmt"
+	"fmt"
 	"io/ioutil"
-  "os"
+	"os"
 )
 
 type EmailCredentials struct {
-    Email string
-    Password string
+	Email    string
+	Password string
 }
 
 func importCredentials(pathToCredentials string) EmailCredentials {
 	raw, err := ioutil.ReadFile(pathToCredentials)
-  if err != nil {
-      fmt.Println(err.Error())
-      os.Exit(1)
-  }
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 
-  var emailCredentials EmailCredentials
-  json.Unmarshal(raw, &emailCredentials)
-  return emailCredentials
+	var emailCredentials EmailCredentials
+	json.Unmarshal(raw, &emailCredentials)
+	return emailCredentials
 }
 
 var Credentials = importCredentials("credentials/credentials.json")
--- main.go
+++ main.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-    "personal_website_backend/credentials"
-    "encoding/json"
-    "fmt"
-    "net/http"
-    "net/smtp"
-    "log"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/smtp"
+	"personal_website_backend/credentials"
 )
 
 type ContactEmailResponse struct {
-    Success bool
+	Success bool
 }
 
 func sendEmail(w http.ResponseWriter, r *http.Request) {
-    // Get our email parameters.
-    r.ParseForm()
-    sender := r.Form["sender"][0]
-    subject := r.Form["subject"][0]
-    message := r.Form["message"][0]
-    // Send out our email.
-    myEmailAddress := credentials.Credentials.Email
-    password := credentials.Credentials.Password
-    msg := fmt.Sprintf("From: %s\nTo: %s\nSubject: %s\n\nSent by %s\n\n%s", sender, myEmailAddress, subject, sender, message)
-    auth := smtp.PlainAuth("", myEmailAddress, password, "smtp.gmail.com")
-    err := smtp.SendMail("smtp.gmail.com:587", auth, myEmailAddress, []string{myEmailAddress}, []byte(msg))
-    contactEmailResponse := ContactEmailResponse { true }
-    if err != nil {
-        log.Printf("smtp error: %s", err)
-        contactEmailResponse = ContactEmailResponse { false }
-    }
-    // Send response to client.
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusCreated)
-    json.NewEncoder(w).Encode(contactEmailResponse)
+	// Get our email parameters.
+	r.ParseForm()
+	sender := r.Form["sender"][0]
+	subject := r.Form["subject"][0]
+	message := r.Form["message"][0]
+	// Send out our email.
+	myEmailAddress := credentials.Credentials.Email
+	password := credentials.Credentials.Password
+	msg := fmt.Sprintf("From: %s\nTo: %s\nSubject: %s\n\nSent by %s\n\n%s", sender, myEmailAddress, subject, sender, message)
+	auth := smtp.PlainAuth("", myEmailAddress, password, "smtp.gmail.com")
+	err := smtp.SendMail("smtp.gmail.com:587", auth, myEmailAddress, []string{myEmailAddress}, []byte(msg))
+	contactEmailResponse := ContactEmailResponse{true}
+	if err != nil {
+		log.Printf("smtp error: %s", err)
+		contactEmailResponse = ContactEmailResponse{false}
+	}
+	// Send response to client.
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(contactEmailResponse)
 }
 
 func main() {
-    http.HandleFunc("/", sendEmail)           // set router
-    err := http.ListenAndServe(":8000", nil)  // set listen port
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	http.HandleFunc("/", sendEmail)          // set router
+	err := http.ListenAndServe(":8000", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
