e773f685cbdc5bf42333c445efd50b7a3731f2c4
Tristan Benavides <tristan@tristans-mbp.home>
2021-06-18 14:38:07 -0400

    Use Kubernetes to deploy backend api.

---
+++ create_gke_cluster.sh
@@ -0,0 +1,17 @@
+while getopts c:z:n: flag
+do
+    case "${flag}" in
+        c) cluster_name=${OPTARG};;
+        z) cluster_zone=${OPTARG};;
+        n) num_nodes=${OPTARG};;
+    esac
+done
+
+response=$(gcloud container clusters describe $cluster_name --zone $cluster_zone || echo "ClusterNotFound")
+if [[ $response = "ClusterNotFound" ]]
+then
+  echo "cluster not found, creating cluster"
+  gcloud container clusters create $cluster_name --num-nodes=$num_nodes --zone=$cluster_zone
+else
+  echo "cluster already exists, skipping creating cluster"
+fi
